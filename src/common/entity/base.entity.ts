105bc13dae2418c60d0b1ba7f39e018e8c2db2b0
jae hyun <1994dbwogus@gmail.com>
2024-10-01 02:49:16 +0900

    feat: TypeormModule + sqlite 접속 설정 완료

---
+++ src/common/entity/base.entity.ts
@@ -0,0 +1,20 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint', name: 'id', comment: 'ID' })
+  readonly id: string;
+
+  @CreateDateColumn({ type: 'datetime', comment: '생성일' })
+  readonly createdAt: Date;
+
+  @UpdateDateColumn({ type: 'datetime', comment: '수정일' })
+  readonly updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'datetime', comment: '삭제일' })
+  deletedAt?: Date | null;
+}
