ecd927fe9b64fc031dc3a135e4821fa30a67515d
jae hyun <1994dbwogus@gmail.com>
2024-10-02 21:10:28 +0900

    feat: AppModule에 에러 헨들링을 위한 전역 인터셉터 신규 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmDataSourceOptions } from './common';
-import { DomainModule } from './domain/domain.module';
+import {
+  ConvertExceptionInterceptor,
+  typeOrmDataSourceOptions,
+} from './common';
+import { DomainModule } from './domain';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...typeOrmDataSourceOptions,
-      // synchronize: true,
-      // dropSchema: true,
+
       logging: true,
     }),
     DomainModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ConvertExceptionInterceptor,
+    },
+  ],
 })
 export class AppModule {}
